fa5cdb5bdd9c66ab8f3427ab5531ab327bb80720
Ali Okan Yüksel <aliokan.yuksel@siyahsapka.org>
2024-04-08 17:08:17 +0300

    Delete get-agent-scans.sh

--- get-agent-scans.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-source function.sh
-
-token="$(curl -s -k -X POST -H "Content-Type: application/json" -d '{"username":"'$(get_username)'","password":"'$(get_password)'"}' $(get_url)/session |jq '.token' |tr -d '"')"
-
-
-curl -s -k -H "Content-Type: application/json" -H 'X-Cookie: token='$token -H "X-APIKey: accesskey=$(get_accesskey); secretkey=$(get_secretkey)" $(get_url)/server/status |jq
