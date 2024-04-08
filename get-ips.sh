47d31fe8a05f301ca4d83862c0b9daaddcb678a5
Ali Okan Yüksel <aliokan.yuksel@siyahsapka.org>
2024-04-08 17:07:10 +0300

    Update get-ips.sh

--- get-ips.sh
+++ get-ips.sh
@@ -14,7 +14,7 @@ for i in $(seq 1 5); do
 	seconds=$(expr $i \* 60)
 	searchstring=$(TZ=UTC date '+%d %b %Y %H:%M' -d "$seconds seconds ago")
 	echo $searchstring
-	grep agentScan /opt/sc/admin/logs/202404.log | grep "$searchstring" | grep 'has acquired Agent Scan' |sed -r "s#.+'(.+?)'.+#\1#" |sort |uniq |while read scanname; do
+	grep agentScan $logfile | grep "$searchstring" | grep 'has acquired Agent Scan' |sed -r "s#.+'(.+?)'.+#\1#" |sort |uniq |while read scanname; do
 		echo scanName=$scanname
 		scanid=$(./get-agent-group-id.sh $scanname)
 		echo scanId=$scanid
