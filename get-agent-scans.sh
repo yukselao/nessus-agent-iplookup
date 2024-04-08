3530a3619f02968d999ef7e0debf3bc5f9e7db6e
Ali Okan Yüksel <aliokan.yuksel@siyahsapka.org>
2024-04-08 17:06:34 +0300

    Update get-agent-scans.sh

--- get-agent-scans.sh
+++ get-agent-scans.sh
@@ -3,7 +3,7 @@
 
 source function.sh
 
-token="$(curl -s -k -X POST -H "Content-Type: application/json" -d '{"username":"nessusadmin","password":"Tenable123!Tenable123!"}' $(get_url)/session |jq '.token' |tr -d '"')"
+token="$(curl -s -k -X POST -H "Content-Type: application/json" -d '{"username":"'$(get_username)'","password":"'$(get_password)'"}' $(get_url)/session |jq '.token' |tr -d '"')"
 
 
 curl -s -k -H "Content-Type: application/json" -H 'X-Cookie: token='$token -H "X-APIKey: accesskey=$(get_accesskey); secretkey=$(get_secretkey)" $(get_url)/server/status |jq
